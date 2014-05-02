bced813cf1e67961f9dcbe6c0cae4df7a530e213
Raghotham S <raghotham.s@gmail.com>
2014-05-02 14:31:39 +0530

    Solving the problem right
    A clean mind is always necessary to solve even a simple problem :relaxed:

--- chocolatefeast.py
+++ chocolatefeast.py
@@ -1,26 +1,19 @@
 
 T = int(raw_input())
-
 for i in range (0,T):
     N,C,M = [int(x) for x in raw_input().strip().split(' ')]
     
     total_choc = 0
     chocs = 0
-    
     if N > C:
-        chocs = N / C
-        rem = N % C
-        total_choc += chocs
+       
+        total_choc = N / C
+        wrappers = total_choc
         
-        while chocs >= M :
-            chocs = chocs / M            
-            rem = chocs % M
-            total_choc += chocs
-            
-        while (rem+chocs) >= M :
-            chocs = (rem+chocs) / M            
-            rem = (rem+chocs) % M
-            total_choc += chocs
+        while wrappers >= M:
+            chocs = wrappers / M
+            wrappers = (wrappers % M) + chocs
+            total_choc = total_choc + chocs
         
         print total_choc
         
